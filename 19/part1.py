48a26c5b4694ca6424bbefdc3ea7b680b6d49f82
Vladimir Kazanov <vekazanov@gmail.com>
2026-01-07 12:10:05 +0000

    day 19, part2

--- 19/part1.py
+++ 19/part1.py
@@ -3,8 +3,8 @@ import pprint
 from util import cstr, COLOR, FORMAT
 from dataclasses import dataclass
 
-# inp = open("input-test.txt")
-inp = open("input.txt")
+# inp = open("input-test-1.txt")
+inp = open("input-1.txt")
 
 @dataclass
 class Seq:
@@ -40,10 +40,7 @@ def parse_seq(seq_line):
 for line in inp:
     line = line.strip()
     if not line: break
-    # print("rule: ", line)
     parse(line)
-
-# pprint.pp(rules)
 
 def expand_rule(rule):
     match rule:
@@ -60,12 +57,12 @@ def expand_rule(rule):
     return rule
 
 rule = expand_rule(rules[0])
-# pprint.pp(rule)
 
 def check(line, rule, i=0):
     match rule:
         case str(r):
             return r == line[i], i + 1
+
         case Seq(rule_seq):
             next_i = i
             for r_i, r in enumerate(rule_seq):
@@ -73,31 +70,17 @@ def check(line, rule, i=0):
                 if not this_res:
                     return False, next_i
             return True, next_i
+
         case Alt(rule_alt):
             for r in rule_alt:
                 this_res, next_i = check(line, r, i)
                 if this_res:
                     return True, next_i
             return False, i
+
         case _:
             assert False
     return False, i
-
-# print(check("aaa", Seq(["a", "a", "a"])))
-# print(check("aaa", Seq(["a", "a", "b"])))
-# print(check("aab", Seq([Seq(["a", "a"]), "b"])))
-# print(check("aab", Seq([Seq(["a", "a"]), "a"])))
-# print(check("abc", Seq([Alt(["a", "b"]), "b", "c"])))
-# print(check("abc", Seq([Alt(["a", "b"]), "b", "d"])))
-# print(check("aab", [["a", "a"], "a"]))
-# print(check("baa", ["a", "a", "a"]))
-# print(check("aaa", ["a", "a"]))
-# print(check("aa", ["a", "a", "a"]))
-# print(check("baa", [["a", "b"], "a", "a"]))
-# print(check("baa", [["b", "a"], "a", "a"]))
-
-# print(check("aba", [[["a"], ["b"]], "a", "a"]))
-
 
 counter = 0
 for line in inp:
--- 19/part2.py
+++ 19/part2.py
@@ -1,4 +1,101 @@
 from io import StringIO
+import pprint
 from util import cstr, COLOR, FORMAT
+from dataclasses import dataclass
 
-inp = open("input.txt")
+# inp = open("input-test-2.txt")
+inp = open("input-2.txt")
+
+@dataclass
+class Rule:
+    value: int
+    def __init__(self, value):
+        self.value = value
+
+@dataclass
+class Seq:
+    value: list
+    def __init__(self, value):
+        self.value = value
+
+@dataclass
+class Alt:
+    value: list
+    def __init__(self, value):
+        self.value = value
+
+all_rules = {}
+
+def parse(rule_line):
+    idx, right = rule_line.split(": ")
+    idx = int(idx)
+    if " | " not in right:
+        all_rules[idx] = Seq(parse_seq(right))
+    else:
+        all_rules[idx] = Alt([Seq(parse_seq(alt)) for alt in right.split(" | ")])
+
+def parse_seq(seq_line):
+    res = []
+    for c in seq_line.split(" "):
+        if c.isnumeric():
+            res.append(Rule(int(c)))
+        else:
+            res.append(c[1:-1])
+    return res
+
+for line in inp:
+    line = line.strip()
+    if not line: break
+    parse(line)
+
+def check(line, rule, i, rules):
+    match rule:
+        case str(r):
+            if i < len(line) and r == line[i]:
+                return True, [i + 1]
+            else:
+                return False, [i]
+
+        case Seq(rule_seq):
+            iss = [i]
+            for r in rule_seq:
+                if not iss:
+                    return False, [i]
+                next_iss = []
+                for next_i in iss:
+                    this_res, this_iss = check(line, r, next_i, rules)
+                    if this_res:
+                        next_iss.extend(this_iss)
+                iss = next_iss
+            return True, next_iss
+
+        case Alt(rule_alt):
+            res_iss = []
+            for r in rule_alt:
+                this_res, next_iss = check(line, r, i, rules)
+                if this_res:
+                    res_iss.extend(next_iss)
+            if res_iss:
+                return True, res_iss
+            else:
+                return False, [i]
+
+        case Rule(rule_i):
+            this_res, next_iss = check(line, rules[rule_i], i, rules)
+            if this_res:
+                return True, next_iss
+            return False, [i]
+
+        case _:
+            print("invalid: ", rule)
+            assert False
+
+counter = 0
+for line in inp:
+    line = line.strip()
+    res, iss = check(line, all_rules[0], 0, all_rules)
+    if res and any(i == len(line) for i in iss):
+        counter += 1
+
+print(counter)
+assert(counter == 267)
